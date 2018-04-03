e87e6bfc5551bbe7f855490a7a122ee197fd1a28
Zyris Shakir <zeyeland@gmail.com>
2018-04-03 14:04:20 -0500

    Added the ability to reject unauthorized services directives

--- re.sh
+++ re.sh
@@ -9,13 +9,17 @@ then
     exit 1
 fi
 
-# Move the current execution state to the proper directory
-cd /etc/apache2/sites-available
+if [ "$COMMAND" == "reload" || "$COMMAND" == "restart" ]
+then
+    # Move the current execution state to the proper directory
+    cd /etc/apache2/sites-available
 
-# Disable a vhost configuration
-sudo a2dissite "$CONFIG"
-sudo service apache2 "$COMMAND"
+    # Disable a vhost configuration
+    sudo a2dissite "$CONFIG"
+    sudo service apache2 "$COMMAND"
 
-# Enable a vhost configuration
-sudo a2ensite "$CONFIG"
-sudo service apache2 "$COMMAND"
+    # Enable a vhost configuration
+    sudo a2ensite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+fi
+
